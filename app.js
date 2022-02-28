11f7101a485ca62c2cb93542425ebfd00c69ff61
Asim Riaz <asimriaz@szabist.net.pk>
2022-02-28 13:21:45 +0500

    Previous topic

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
+const logger = require('./logger')
+
 console.log("Hello World");
 console.log('object')
+
+logger.doLog('456')
---
+++ logger.js
@@ -0,0 +1,7 @@
+
+exports.doLog = function(msg){
+    //console.log(`The value of a : ${a}`);
+    console.log(`<<${msg}>>`)
+}
+
+//module.exports.log = doLog
